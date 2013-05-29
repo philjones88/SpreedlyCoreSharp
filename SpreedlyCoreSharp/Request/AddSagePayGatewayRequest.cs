0b32e0885c63418f89b7ad856a6ab94008a8e4f0
Phil Jones <phil.jones88@gmail.com>
2013-05-29 16:32:11 +0100

    Added add gateway request for the SagePay gateway

---
+++ SpreedlyCoreSharp/Request/AddSagePayGatewayRequest.cs
@@ -0,0 +1,19 @@
+﻿using System.Xml.Serialization;
+
+namespace SpreedlyCoreSharp.Request
+{
+    [XmlRoot("gateway")]
+    public class AddSagePayGatewayRequest
+    {
+        public AddSagePayGatewayRequest()
+        {
+            GatewayType = "sage_pay";
+        }
+
+        [XmlElement("gateway_type")]
+        public string GatewayType { get; set; }
+
+        [XmlElement("login")]
+        public string Login { get; set; }
+    }
+}
