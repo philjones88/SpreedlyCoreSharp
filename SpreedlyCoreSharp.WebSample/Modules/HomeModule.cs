aa777c57e5b9f63df1effc5ad2581e6d0e2a8fb4
Phil Jones <phil.jones88@gmail.com>
2014-07-12 12:23:35 +0100

    Upgrading web sample to latest Nancy

--- SpreedlyCoreSharp.WebSample/Modules/HomeModule.cs
+++ SpreedlyCoreSharp.WebSample/Modules/HomeModule.cs
@@ -6,10 +6,7 @@ namespace SpreedlyCoreSharp.WebSample.Modules
     {
         public HomeModule()
         {
-            Get["/"] = _ =>
-            {
-                return View["Home"];
-            };
+            Get["/"] = _ => View["Home"];
         }
     }
 }
