13d189925def80ac9d02fa9fec14ed3766ae3ab9
ithielnor <ithielnor@gmail.com>
2015-07-29 11:40:47 -0400

    Add Stripe and Authorize.Net gateways

---
+++ SpreedlyCoreSharp/Request/StripeGatewayRequest.cs
@@ -0,0 +1,16 @@
+﻿using RestSharp.Serializers;
+
+namespace SpreedlyCoreSharp.Request
+{
+    [SerializeAs(Name = "gateway")]
+    public class StripeGatewayRequest : BaseGatewayRequest
+    {
+        public StripeGatewayRequest()
+        {
+            GatewayType = "stripe";
+        }
+
+        [SerializeAs(Name = "login")]
+        public string Login { get; set; }
+    }
+}
